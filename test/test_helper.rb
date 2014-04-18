2d981e6ed401d46325ab636150afafd2cefc5fc6
Youri van der Lans <youri@itflows.nl>
2014-04-18 13:04:02 +0200

    Created a cardgate_connection test helper method which can be used to stub faraday connections.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV['RAILS_ENV'] = 'test'
 
 require 'test/unit'
+require 'faraday'
+require 'fixtures'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
@@ -9,3 +11,13 @@ require 'cardgate'
 # For generators
 require 'rails/generators/test_case'
 require 'generators/cardgate/install_generator'
+
+def cardgate_connection
+  @connection ||= Faraday.new do |faraday|
+    faraday.response :json
+
+    faraday.adapter :test do |stubs|
+      yield(stubs)
+    end 
+  end
+end
