0b149cb2fbeb0b391e1917798d7908e29ea72625
Youri van der Lans <youri@itflows.nl>
2014-04-18 13:04:19 +0200

    Added fixtures for cardgate responses.

---
+++ test/fixtures.rb
@@ -0,0 +1,6 @@
+class CardgateFixtures
+
+  ISSUERS_LIST = '{"issuers":[{"list":"Short", "name":"Test Issuer", "id":121}, {"list":"Short", "name":"Test Issuer 2", "id":151}, {"list":"Long", "name":"Test Issuer 3", "id":171}]}'
+  ISSUERS_LIST_EMPTY = '{"issuers":[]}'
+
+end
